5a1ba0b8b78b9b68dc0d8bbd751e7a03e238dff5
r0m1 <romain.bertholon@gmail.com>
2021-02-25 15:47:33 +0100

    Add middleware to protect authenticated routes

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userCtr = require('../controllers/user');
 
-router.post('/signup', userCtr.login);
+router.post('/signup', userCtr.signup);
 router.post('/login', userCtr.login);
 
 module.exports = router;
